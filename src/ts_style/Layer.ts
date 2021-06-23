9c712633112f1683f4750d55705e1e43601b51bf
darkgraycat <jburyanov@gmail.com>
2021-06-23 10:45:43 +0300

    update

--- src/ts_style/Layer.ts
+++ src/ts_style/Layer.ts
@@ -2,18 +2,26 @@ import ILayer from "./interfaces/ILayer"
 
 export default class Layer implements ILayer {
 
-  private flavor: string = 'empty'
-  private filled: boolean = false
+  // private flavor: string = 'empty'
+  // private filled: boolean = false
 
-  getFlavor(): string {
+  private flavor: string
+  private filled: boolean
+
+  public constructor() {
+    this.flavor = 'empty'
+    this.filled = false
+  }
+
+  public getFlavor(): string {
     return this.flavor
   }
 
-  isFilled(): boolean {
+  public isFilled(): boolean {
     return this.filled
   }
 
-  fill(flavor: string): void {
+  public fill(flavor: string): void {
     if (this.filled) return
     this.flavor = flavor
     this.filled = true
